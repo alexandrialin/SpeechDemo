13d98feb0e6692a48bf1ac829860eacbb0ff567c
alexandrialin <alin92@uwo.ca>
2021-06-07 04:03:46 -0400

    Project ready

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def index():
     return render_template('index.html', transcript = transcript)
 
 if __name__ =="__main__":
-    app.run(debug=True, threaded= True)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded= True)
